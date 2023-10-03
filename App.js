f9987d283d4e310e09bf1cbb4ff5e6e878cc7dd2
Your Name <you@example.com>
2023-10-03 20:08:13 +0300

    bottom tabs navigaatio

--- App.js
+++ App.js
@@ -1,30 +1,75 @@
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Gameboard from "./components/Gameboard";
+import Scoreboard from "./components/Scoreboard";
 import { AppContext } from "./components/AppContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  const [started, setStarted] = useState(false);
-
   return (
-    <AppContext.Provider value={{ started, setStarted }}>
-      <View style={styles.container}>
-        <Header />
-        {started ? <Gameboard /> : <Home />}
-        <Footer />
-      </View>
-    </AppContext.Provider>
+    <NavigationContainer>
+      <Tab.Navigator
+        // initialRouteName="Feed"
+        sceneContainerStyle={{ backgroundColor: "transparent" }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = focused ? "information" : "information-outline";
+            } else if (route.name === "Gameboard") {
+              iconName = focused ? "dice-multiple" : "dice-multiple-outline";
+            } else if (route.name === "Scoreboard") {
+              iconName = focused ? "view-list" : "view-list-outline";
+            }
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          },
+          tabBarActiveTintColor: "steelblue",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          // options={{ tabBarStyle: { display: none } }}
+        />
+        <Tab.Screen
+          name="Gameboard"
+          component={Gameboard}
+          // options={{ tabBarStyle: { display: none } }}
+        />
+        <Tab.Screen
+          name="Scoreboard"
+          component={Scoreboard}
+          //options={{ tabBarStyle: { display: none } }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// export default function App() {
+//   const [started, setStarted] = useState(false);
+
+//   return (
+//     <AppContext.Provider value={{ started, setStarted }}>
+//       <View style={styles.container}>
+//         <Header />
+//         {started ? <Gameboard /> : <Home />}
+//         <Footer />
+//       </View>
+//     </AppContext.Provider>
+//   );
+// }
--- components/Home.js
+++ components/Home.js
@@ -4,14 +4,14 @@ import styles from "../styles/style";
 import { AppContext } from "./AppContext";
 
 export default function Home() {
-  const { started, setStarted } = useContext(AppContext);
+  // const { started, setStarted } = useContext(AppContext);
   return (
     <View>
-      <Text>For scoreboard enter your name: </Text>
+      {/* <Text>For scoreboard enter your name: </Text>
       <TextInput></TextInput>
       <Pressable style={styles.button} onPress={() => setStarted(true)}>
         <Text style={styles.buttonText}>OK</Text>
-      </Pressable>
+      </Pressable> */}
     </View>
   );
 }
